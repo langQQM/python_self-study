8a066099b80e1df88d7ffadf8f5685f541ac49d8
pandaQ <fury886@163.com>
2023-11-07 23:15:42 +0900

    for example's update

--- eg02_for.py
+++ eg02_for.py
@@ -1,3 +1,31 @@
 shows=["Apple", "Peach", "Watermelon"]
 for show in shows:
     print(show)
+
+# continue
+for i in range(1,6):
+    if i==3:
+        continue
+    print(i)
+
+# break
+for j in range(0,100):
+    print(j)
+    break
+
+# while example01
+qs=["what is your name?",
+    "what is your fav. color?",
+    "what is your quest?"]
+n=0
+while True:
+    print("type q to quit")
+    a=input(qs[n])
+    if a=="q":
+        break
+    n=(n+1)%len(qs)
+
+# while example02
+while input("y or n?")!="n":
+    for i in range(1,6):
+        print(i)
